1d1e8b5b9153f13fd192ce2dfaa71febaead958b
pgrm <peter.grman@gmail.com>
2012-07-23 14:21:30 +0200

    updated copy/paste errors and requirements domain behaviour / structure

---
+++ src/main/java/org/openengsb/domain/requirement/Comment.java
@@ -0,0 +1,55 @@
+/**
+ * Licensed to the Austrian Association for Software Tool Integration (AASTI)
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership. The AASTI licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.openengsb.domain.requirement;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Peter
+ * Date: 06.07.12
+ * Time: 15:07
+ * To change this template use File | Settings | File Templates.
+ */
+public class Comment {
+    private Person person;
+    private String dateTime;
+    private String content;
+
+    public Person getAuthor() {
+        return person;
+    }
+
+    public void setAuthor(Person person) {
+        this.person = person;
+    }
+
+    public String getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(String dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
---
+++ src/main/java/org/openengsb/domain/requirement/events/RequirementStatusChangedEvent.java
@@ -0,0 +1,11 @@
+package org.openengsb.domain.requirement.events;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Peter
+ * Date: 23.07.12
+ * Time: 12:28
+ * To change this template use File | Settings | File Templates.
+ */
+public class RequirementStatusChangedEvent {
+}
